8abb3a9d495db4025636bd7fb0218d8cd5a591b9
janardhani99 <48481633+janardhani99@users.noreply.github.com>
2020-05-24 22:53:42 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Provinsi Bali</title>
+    <title>Peta Sebaran Covid-19 Provinsi Bali</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Krub" rel="stylesheet">
